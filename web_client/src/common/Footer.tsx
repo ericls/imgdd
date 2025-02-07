f921bcc4e836783c9945c38c3ee5f9cd9009b548
Shen Li <dustet@gmail.com>
2025-02-06 23:38:58 -0500

    footer; disable auto docker build

--- web_client/src/common/Footer.tsx
+++ web_client/src/common/Footer.tsx
@@ -3,6 +3,18 @@ import React from "react";
 import { Slot } from "~src/lib/slot";
 import { SECONDARY_TEXT_COLOR_DIM } from "~src/ui/classNames";
 
+const DefaultFooterContent = (
+  <ul className="flex space-x-1">
+    <li>
+      <a href="https://hub.docker.com/r/ericls/imgdd">Docker</a>
+    </li>
+    <li className="hidden md:inline select-none">|</li>
+    <li>
+      <a href="http://imgdd.com">IMGDD.COM 🇨🇦</a>
+    </li>
+  </ul>
+);
+
 export function Footer({ center = false }: { center?: boolean }) {
   return (
     <div
@@ -17,7 +29,7 @@ export function Footer({ center = false }: { center?: boolean }) {
     >
       <div>
         <span slot-id="footer-content" className="opacity-50">
-          <Slot id="footer-content" fallback="IMGDD.COM 🇨🇦" />
+          <Slot id="footer-content" fallback={DefaultFooterContent} />
           {/* IMGDD.COM 🇨🇦 */}
         </span>
       </div>
