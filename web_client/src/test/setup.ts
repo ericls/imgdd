348120af036312de448fabe8a9762d328bedfca9
Shen <shen@shenli.ca>
2026-06-07 22:50:30 +0000

    mock animations

--- web_client/src/test/setup.ts
+++ web_client/src/test/setup.ts
@@ -1,6 +1,10 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
+import { configMocks, mockAnimationsApi } from "jsdom-testing-mocks";
+import { afterAll, afterEach } from "vitest";
+
+configMocks({ afterAll, afterEach });
+mockAnimationsApi();
 
 afterEach(() => {
   cleanup();
